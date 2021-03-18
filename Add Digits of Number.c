0bc8c2806f2a5a9a501286ea9a4d9d0437664563
JAYASREE T <jayasreet2112@gmail.com>
2021-03-18 15:37:50 +0530

    Add Digits of Number

---
+++ Add Digits of Number.c
@@ -0,0 +1,14 @@
+// 2.Add the digits of a number and display.
+#include<stdio.h>
+int main(){
+	long int n,count=0,rem=0,sum=0;
+	scanf("%ld",&n);
+	while(n>0){
+		rem=n%10;
+		sum=sum+rem;
+		n=n/10;
+		count++;
+	}
+	printf("No. of digits are: %ld\n",count);
+	printf("The sum is: %ld\n",sum);
+}
